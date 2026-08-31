d92360c5aa3d16ed6b19f0c1b7f4e95d229d3ec5
creatorsg <soninseon2004@gmail.com>
2026-08-31 16:08:53 +0900

    save

---
+++ src/main/java/com/rookies6/myspringboot4project/runner/MyRunner.java
@@ -0,0 +1,43 @@
+package com.rookies6.myspringboot4project.runner;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Consumer;
+
+@Component
+public class MyRunner implements ApplicationRunner {
+    @Value("${spring.application.name}")
+    String applicationName;
+
+    @Value("${myboot.name}")
+    private String name;
+
+    @Value("${myboot.age}")
+    private int age;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("MyRunner run() 호출됨!!");
+        System.out.println("Application Name = " + applicationName);
+
+        //Consumer 인터페이스를 Anonymous Inner Class 로 표현
+        args.getOptionNames().forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println("arg name = " + s);
+            }
+        });
+
+        // args.getOptionNames() 메서드의 리턴타입 Set<String>
+        //Iterable 의 forEach(Consumer)
+        //Consumer의 추상메서드 void accept(T t)
+        //Consumer 인터페이스를 람다식으로 표현
+        args.getOptionNames().forEach(name -> System.out.println(name));
+
+        //Consumer 인터페이스를 Method Reference 로 표현
+        args.getOptionNames().forEach(System.out::println);
+    }
+}
